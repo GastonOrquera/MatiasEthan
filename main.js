78e2670a60c06d04d5282541a53db1ed00f22190
GastonOrquera <gastonorquera29@gmail.com>
2025-08-29 02:57:35 -0300

    modificaciones

---
+++ main.js
@@ -0,0 +1,31 @@
+// Contador de +136
+
+let counter = document.getElementById("counter");
+let value = 0;
+let endValue = 136;
+let speed = 20; // menor = más rápido
+
+let interval = setInterval(() => {
+    value++;
+    counter.textContent = `+${value}`;
+    if (value === endValue) {
+        clearInterval(interval);
+    }
+}, speed);
+
+// Menu hamburguesa}
+
+let hamburger = document.querySelector('.hamburger');
+let nav = document.querySelector('nav');
+let navAll = document.querySelectorAll('a');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.add('active');
+
+
+});
+
+
+
+
+
